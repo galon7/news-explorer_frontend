95840110a92234c19b910cb652478ebb405fade8
Alon G <agoykman@gmail.com>
2022-01-29 14:03:40 +0200

    feat: input empty message

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SearchForm.css';
 
 function SearchForm() {
+  const [inputValue, setInputValue] = useState('');
+  const [placeHolder, setPlaceHolder] = useState('Enter topic');
+
+  function handleInputChange(e) {
+    setInputValue(e.target.value);
+  }
+
+  function resetPlaceHolder() {
+    setPlaceHolder('Enter topic');
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!inputValue) setPlaceHolder('Please enter topic');
+  }
+
   return (
     <form className="search-form">
-      <input className="search-form__input" placeholder="Enter topic" />
-      <button className="search-form__button">Search</button>
+      <input
+        className={`searchForm__input ${
+          placeHolder === 'Please enter topic' && 'searchForm__input-empty'
+        }`}
+        placeholder={placeHolder}
+        onChange={handleInputChange}
+        value={inputValue}
+        onFocus={resetPlaceHolder}
+      />
+      <button type="submit" className="search-form__button" onClick={handleSubmit}>
+        Search
+      </button>
     </form>
   );
 }
