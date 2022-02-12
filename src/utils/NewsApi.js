db3fd71c6a2e2339d548dd909d9659071bbab7c0
Alon G <agoykman@gmail.com>
2022-02-12 18:55:55 +0200

    update newsApi address

--- src/utils/NewsApi.js
+++ src/utils/NewsApi.js
@@ -7,7 +7,7 @@ export const pageSize = 100;
 
 export function getNewsFromApi(input, apiKey, from, to, pageSize) {
   return fetch(
-    `https://newsapi.org/v2/everything?q=${input}&apiKey=${apiKey}&from=${from}&to=${to}&pageSize=${pageSize}&language=en`
+    `https://nomoreparties.co/news/v2/top-headlines?q=${input}&apiKey=${apiKey}&from=${from}&to=${to}&pageSize=${pageSize}&language=en`
   ).then((res) => {
     if (!res.ok) {
       return Promise.reject(`Error: ${res.status}`);
